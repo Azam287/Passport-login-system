d9eb86da10395e63a52ae24cd6a7c9a570b5e37c
Azam287 <mohdazamnoor28@gmail.com>
2021-02-27 14:22:49 +0530

    adding git ignore

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+app.set('view-engine', 'ejs');
+
 app.get('/',(req,res)=>{
-    res.render('index.ejs')
+    res.render('index.ejs');
 });
 
+
+//creating server
 app.listen(3000);
